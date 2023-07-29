32e5515e646643396293c8b390b1f4bbd5a2bba1
Jesper Olsson <skoldfold@gmail.com>
2023-07-29 11:32:20 +0200

    Remove comments

--- 6-kyu/give-me-a-diamond.js
+++ 6-kyu/give-me-a-diamond.js
@@ -1,5 +1,4 @@
 const diamond = (n) => {
-
   if (n % 2 === 0 || n < 1) {
     return null
   }
@@ -8,46 +7,21 @@ const diamond = (n) => {
   let whitespace = (n + 1) / 2
 
   for (let i = 1; i <= n; i += 2) {
-
     if (whitespace !== 0) {
       whitespace -= 1
     }
-
+    
     diamond += " ".repeat(whitespace)
     diamond += "*".repeat(i) + "\n"
   }
 
-  whitespace = 1;
+  let counter = 0
 
   for (let i = n - 2; i >= 0; i -= 2) {
-    diamond += " ".repeat(whitespace)
+    counter += 1
+    diamond += " ".repeat(counter)
     diamond += "*".repeat(i) + "\n"
-    whitespace += 1
   }
 
   return diamond
-}
-console.log(diamond(1))
-//if even or negative return null
-//when loop has reached n then do -2 until 1
-/*
-   *
-  ***
- *****
-*******
- *****
-  ***
-   *
-  
-     *
-    ***
-   ***** 
-  *******
- *********
-***********
- *********
-  *******
-   *****
-    *** 
-     * 
-*/
+} 
