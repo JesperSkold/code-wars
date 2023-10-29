a5951a6527859ca7d1233d2d0d36cb30b0d83e61
Jesper Olsson <skoldfold@gmail.com>
2023-10-29 12:30:04 +0100

    Small amend

--- 5-kyu/not-very-secure.js
+++ 5-kyu/not-very-secure.js
@@ -1,2 +1,2 @@
 // https://www.codewars.com/kata/526dbd6c8c0eb53254000110/train/javascript
-const alphanumeric = string => /^[a-z0-9.-]+$/gi.test(string)
+const alphanumeric = string => /^[a-z0-9]+$/i.test(string)
